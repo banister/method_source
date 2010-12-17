70490396f06dae63ba4c7166f3cb00e58e457f15
Adam Sanderson <netghost@gmail.com>
2010-12-17 10:55:16 -0800

    Add support for extracting comments from methods.

--- lib/method_source.rb
+++ lib/method_source.rb
@@ -34,6 +34,33 @@ module MethodSource
     file
   end
   
+  # Helper method responsible for opening source file and buffering up
+  # the comments for a specified method. Defined here to avoid polluting 
+  # `Method` class.
+  # @param [Array] source_location The array returned by Method#source_location
+  # @return [String] The comments up to the point of the method.
+  def self.comment_helper(source_location)
+    return nil if !source_location.is_a?(Array)
+    
+    file_name, line = source_location
+    file = File.open(file_name)
+    buffer = ""
+    (line - 1).times do
+      line = file.readline
+      # Add any line that is a valid ruby comment, 
+      # but clear as soon as we hit a non comment line.
+      if (line =~ /^\s*#/) || (line =~ /^\s*$/)
+        buffer << line
+      else
+        buffer.clear
+      end
+    end
+    
+    buffer.strip
+  ensure
+    file.close if file
+  end
+  
   # This module is to be included by `Method` and `UnboundMethod` and
   # provides the `#source` functionality
   module MethodExtensions
@@ -70,6 +97,28 @@ module MethodSource
     ensure
       file.close if file
     end
+    
+    # Return the comments associated with the method as a string.
+    # (This functionality is only supported in Ruby 1.9 and above)
+    # @return [String] The method's comments as a string
+    # @example
+    #  Set.instance_method(:clear).comment.display
+    #  =>
+    #     # Removes all elements and returns self.
+    def comment
+      file = nil
+      
+      if respond_to?(:source_location)
+        comment = MethodSource.comment_helper(source_location)
+        
+        raise "Cannot locate source for this method: #{name}" if !comment
+      else
+        raise "Method#comment not supported by this Ruby version (#{RUBY_VERSION})"
+      end
+
+      comment
+    end
+    
   end
 end
 
--- test/test.rb
+++ test/test.rb
@@ -4,11 +4,15 @@ require 'bacon'
 require "#{direc}/../lib/method_source"
 
 hello_source = "def hello; :hello; end\n"
+hello_comment = "# A comment for hello"
+lambda_comment = "# This is a comment for MyLambda"
 lambda_source = "MyLambda = lambda { :lambda }\n"
 proc_source = "MyProc = Proc.new { :proc }\n"
 
+# A comment for hello
 def hello; :hello; end
 
+# This is a comment for MyLambda
 MyLambda = lambda { :lambda }
 MyProc = Proc.new { :proc }
 
@@ -23,6 +27,10 @@ describe MethodSource do
     if RUBY_VERSION =~ /1.9/
       it 'should return source for method' do
         method(:hello).source.should == hello_source
+      end
+      
+      it 'should return a comment for method' do
+        method(:hello).comment.should == hello_comment
       end
 
       it 'should raise for C methods' do
@@ -41,9 +49,17 @@ describe MethodSource do
       it 'should return source for proc' do
         MyProc.source.should == proc_source
       end
-
+      
+      it 'should return an empty string if there is no comment' do
+        MyProc.comment.should == ''
+      end
+      
       it 'should return source for lambda' do
         MyLambda.source.should == lambda_source
+      end
+      
+      it 'should return comment for lambda' do
+        MyLambda.comment.should == lambda_comment
       end
     else
       it 'should raise on #source' do
