6a9c769ebfbf8c93513e2ddbba77360a73726a8f
Conrad Irwin <conrad.irwin@gmail.com>
2012-09-11 16:05:15 -0700

    Basic ruby-2.0 support

--- lib/method_source/code_helpers.rb
+++ lib/method_source/code_helpers.rb
@@ -125,7 +125,7 @@ module MethodSource
       def self.===(ex)
         return false unless SyntaxError === ex
         case ex.message
-        when /unexpected (\$end|end-of-file|END_OF_FILE)/, # mri, jruby, ironruby
+        when /unexpected (\$end|end-of-file|end-of-input|END_OF_FILE)/, # mri, jruby, ruby-2.0, ironruby
           /embedded document meets end of file/, # =begin
           /unterminated (quoted string|string|regexp) meets end of file/, # "quoted string" is ironruby
           /missing 'end' for/, /: expecting '[})\]]'$/, /can't find string ".*" anywhere before EOF/, /: expecting keyword_end/, /expecting kWHEN/ # rbx
